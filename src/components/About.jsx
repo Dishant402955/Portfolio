f93d2fc9e41423504a84ab5f722884cc85d1d20f
Dishant radadiya <dishantradadiya3036@gmail.com>
2025-02-24 21:53:40 +0530

    Selection Fixes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,7 +20,7 @@ const AboutSection = () => {
   return (
     <div className="py-20 flex justify-center items-center" id="about">
       <div className="container flex flex-col justify-center items-center">
-        <p className="text-2xl font-bold my-4 lg:my-6">ABOUT ME</p>
+        <p className="text-2xl font-bold my-4 lg:my-6 select-none">ABOUT ME</p>
         <h2 className="text-xl text-zinc-300 mt-2 ">A Glimpse into My World</h2>
         <p className="max-w-[280px] text-zinc-500 text mb-4 text-center">Learn more about who i am, what i do, and what inspires me</p>
         {/* flex-wrap flex justify-center items-start lg lg:my-10 */}
@@ -28,15 +28,15 @@ const AboutSection = () => {
           <div id="toolbox" className="border border-zinc-500 m-2 px-3 py-4 rounded-xl bg-zinc-400/15 flex flex-col w-[95%] max-w-[330px] flex-wrap row-span-2">
             <div>
               <div className="flex gap-1 items-center mb-2 mt-3">
-                <img src={StarIcon} className="bg-amber-50/40 rounded-full mr-2" />
-                <h3 className="text-xl text-zinc-200/90">My Toolbox</h3>
+                <img src={StarIcon} className="bg-amber-50/40 rounded-full mr-2 select-none" />
+                <h3 className="text-xl text-zinc-200/90 select-none">My Toolbox</h3>
               </div>
               <p className="text-zinc-400 my-3 mx-2">Explore the technologies and tools i use to craft my applications</p>
             </div>
             <div id="tools" className="flex flex-wrap gap-4 justify-center items-center my-4 mt-20 max-[900px]:mt-2">
               {tools.map((tool) => {
                 return (
-                  <div className="flex items-center gap-x-4 border w-fit px-4 py-3 border-zinc-500 rounded-lg text-md text-zinc-300" key={tool.id}>
+                  <div className="flex items-center gap-x-4 border w-fit px-4 py-3 border-zinc-500 rounded-lg text-md text-zinc-300 select-none" key={tool.id}>
                     <img src={tool.link} width={25} />
                     <h4>{tool.title}</h4>
                   </div>
@@ -45,22 +45,22 @@ const AboutSection = () => {
             </div>
           </div>
           <div id="reads" className="border border-zinc-500 m-2 px-3 py-4 rounded-xl bg-zinc-400/15 flex flex-col w-[95%] max-w-[330px] flex-wrap">
-            <div className="flex gap-1 items-center mb-2">
+            <div className="flex gap-1 items-center mb-2 select-none">
               <img src={StarIcon} className="bg-amber-50/40 rounded-full mr-2" />
               <h3 className="text-xl text-zinc-200/90">My Reads</h3>
             </div>
             <p className="text-zinc-400 my-3 mx-2">Explore the books that shapes my Life</p>
-            <div className="books flex justify-center items-center my-4">
+            <div className="books flex justify-center items-center my-4 select-none">
               <img src={Book} alt="book" width={135} />
             </div>
           </div>
           <div id="hobbies" className="border border-zinc-500 m-2 px-3 py-4 rounded-xl bg-zinc-400/15 flex flex-col w-[95%] max-w-[330px]">
-            <div className="flex gap-1 items-center mb-2">
+            <div className="flex gap-1 items-center mb-2 select-none">
               <img src={StarIcon} className="bg-amber-50/40 rounded-full mr-2" />
               <h3 className="text-xl text-zinc-200/90">Beyond the Code</h3>
             </div>
             <p className="text-zinc-400 my-3 mx-2">Explore my interests and hobbies beyond the digital realm</p>
-            <div className="books flex justify-center items-center flex-wrap my-4 gap-2" id="hobby">
+            <div className="books flex justify-center items-center flex-wrap my-4 gap-2 select-none" id="hobby">
               <div className="py-2 px-6 bg-zinc-950/35 border border-zinc-600 rounded-xl text-lg">
                 <h4>Reading</h4>
               </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -30,17 +30,17 @@ const ProjectsSection = () => {
   return (
     <div className="py-20 lg:py-24 flex justify-center items-center" id="projects">
       <div className="container flex flex-col justify-center items-center">
-        <p className="text-2xl text-zinc-400 font-bold lg:my-4">REAL WORLD RESULTS</p>
-        <h2 className="text-xl font-extralight text-zinc-300/50 mt-5  lg:my-5">Featured Projects</h2>
+        <p className="text-2xl text-zinc-400 font-bold lg:my-4 select-none">REAL WORLD RESULTS</p>
+        <h2 className="text-xl font-extralight text-zinc-300/50 mt-5  lg:my-5 select-none">Featured Projects</h2>
         <div className="flex sm:flex-col md:flex-row justify-center items-center flex-wrap  lg:my-5">
           {portfolioProjects.map((project) => {
             return (
               <div key={project.id} className="flex flex-col border border-zinc-600 my-4 px-4 py-6 rounded-xl gap-y-4 bg-zinc-700/20 max-w-[300px] mx-4">
                 <div className="flex flex-col">
-                  <span className="text-zinc-500 ">{project.year}</span>
+                  <span className="text-zinc-500 select-none">{project.year}</span>
                   <h3 className="text-xl text-zinc-300">{project.title}</h3>
                 </div>
-                <img src={project.image} alt="project img" className="w-[100%]" />
+                <img src={project.image} alt="project img" className="w-[100%] select-none" />
               </div>
             );
           })}
